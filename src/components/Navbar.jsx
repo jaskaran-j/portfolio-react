650ab3f4a9e6b60db52b7932b4f9c96351e1dd71
Jaskarandeep Singh Jashan <jaskarandeepsinghjashan@gmail.com>
2026-07-14 22:20:08 -0500

    initial commit - routing and navbar setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+  return (
+    <nav>
+      <span>Jas.dev</span>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/projects">Projects</Link>
+      <Link to="/experience">Experience</Link>
+      <Link to="/contact">Contact</Link>
+    </nav>
+  )
+}
+
+export default Navbar
